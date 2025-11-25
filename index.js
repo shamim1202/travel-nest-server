91ae65e24290f8d6c6853a0446524f1be6017dba
shamim1202 <mehedi.farzu@gmail.com>
2025-11-25 12:41:36 +0600

    create get api for show data in different id

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const connectToDB = require("./mongodb");
+const { ObjectId } = require("mongodb");
 
 app.use(express.json());
 app.use(cors());
@@ -31,7 +32,50 @@ app.get("/hotels", async (req, res) => {
     res.send(result);
   } catch (error) {
     console.log(error);
-    res.status(500).send({ error: "Failed to fetch data" });
+    res.status(500).send({ error: "Failed to fetch all hotels/resourts data" });
+  }
+});
+
+app.get("/top-rooms", async (req, res) => {
+  try {
+    const { db } = await connectToDB();
+    const type = req.query.type;
+    const cursor = db.collection("rooms").find({ type }).limit(3);
+    const result = await cursor.toArray();
+    res.send(result);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error: "Failed to fetch rooms data" });
+  }
+});
+
+app.get("/rooms", async (req, res) => {
+  try {
+    const { db } = await connectToDB();
+    const query = {};
+    const cursor = db.collection("rooms").find(query).limit(9);
+    const result = await cursor.toArray();
+    res.send(result);
+  } catch (error) {
+    console.log(error);
+    req.status(500).send({ error: "Failed to fetch rooms data" });
+  }
+});
+
+app.get("/rooms/:id", async (req, res) => {
+  try {
+    const { db } = await connectToDB();
+    const id = req.params.id;
+    if (!ObjectId.isValid(id)) {
+      return res.status(400).send({ error: "Invalid Room ID" });
+    }
+    const query = { _id: new ObjectId(id) };
+    const result = await db.collection("rooms").findOne(query);
+    if (!result) return res.status(404).send({ error: "Room not found" });
+    res.send(result);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error: "Failed to fetch room" });
   }
 });
 
